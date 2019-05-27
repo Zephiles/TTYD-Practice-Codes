85dade3e651ffb7f10f04ef9d53d70b0bf30ee4e
Zephiles <cae222@yahoo.com>
2019-05-27 02:17:03 -0500

    DisableDPadOptionsDisplay.s - Don't back up r3
    r3 doesn't need to be backed up.

--- ttyd-tools/rel/source/assembly/DisableDPadOptionsDisplay.s
+++ ttyd-tools/rel/source/assembly/DisableDPadOptionsDisplay.s
@@ -2,9 +2,8 @@
 .global BranchBackDisableDPadOptionsDisplay
 
 StartDisableDPadOptionsDisplay:
-stwu %sp,-0x10(%sp)
-stmw %r30,0x8(%sp)
-mr %r30,%r3
+stwu %sp,-0xC(%sp)
+stw %r31,0x8(%sp)
 mr %r31,%r4
 
 mr %r3,%r0 # unkVar
@@ -13,10 +12,9 @@ bl disableDPadOptionsDisplay
 # Check the returned bool
 cmpwi %r3,0 # False
 
-mr %r3,%r30
 mr %r4,%r31
-lmw %r30,0x8(%sp)
-addi %sp,%sp,0x10
+lwz %r31,0x8(%sp)
+addi %sp,%sp,0xC
 
 BranchBackDisableDPadOptionsDisplay:
 b 0
