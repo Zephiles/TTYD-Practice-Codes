51d711d352f3cbae907efaeda2b8df9e88e97aad
Zephiles <cae222@yahoo.com>
2021-01-04 19:29:48 -0600

    Define a heap id to indicate which heap to allocate memory from

---
+++ ttyd-tools/rel/include/cxx.h
@@ -0,0 +1,4 @@
+#pragma once
+
+// Define a heap id to indicate which heap to allocate memory from
+#define HEAP_ID 0
--- ttyd-tools/rel/source/cxx.cpp
+++ ttyd-tools/rel/source/cxx.cpp
@@ -1,28 +1,30 @@
+#include "cxx.h"
+
 #include <ttyd/memory.h>
 
 #include <cstddef>
 
 void *operator new(std::size_t size)
 {
-    return ttyd::memory::__memAlloc(0, size);
+    return ttyd::memory::__memAlloc(HEAP_ID, size);
 }
 void *operator new[](std::size_t size)
 {
-    return ttyd::memory::__memAlloc(0, size);
+    return ttyd::memory::__memAlloc(HEAP_ID, size);
 }
 void operator delete(void *ptr)
 {
-    return ttyd::memory::__memFree(0, ptr);
+    return ttyd::memory::__memFree(HEAP_ID, ptr);
 }
 void operator delete[](void *ptr)
 {
-    return ttyd::memory::__memFree(0, ptr);
+    return ttyd::memory::__memFree(HEAP_ID, ptr);
 }
 void operator delete(void *ptr, std::size_t size)
 {
-    return ttyd::memory::__memFree(0, ptr);
+    return ttyd::memory::__memFree(HEAP_ID, ptr);
 }
 void operator delete[](void *ptr, std::size_t size)
 {
-    return ttyd::memory::__memFree(0, ptr);
+    return ttyd::memory::__memFree(HEAP_ID, ptr);
 }
