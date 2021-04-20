6b6a049317477981b612a9aeb7a36c742d4bfa9f
Zephiles <cae222@yahoo.com>
2021-04-20 11:21:07 -0500

    Add function for undoing stuff done by hookFunction

--- ttyd-tools/rel/include/patch.h
+++ ttyd-tools/rel/include/patch.h
@@ -53,4 +53,39 @@ Func hookFunction(Func function, Dest destination)
     return reinterpret_cast<Func>(trampoline);
 }
 
+template<typename Func>
+Func unhookFunction(Func trampoline)
+{
+    uint32_t *instructions = reinterpret_cast<uint32_t *>(trampoline);
+    
+    // Restore the original instruction
+    uint32_t instructionAddress = reinterpret_cast<uint32_t>(&instructions[1]);
+    uint32_t branchInstruction = instructions[1];
+    uint32_t branchLength;
+    uint32_t *address;
+    
+    if (branchInstruction >= 0x4A000000)
+    {
+        // Branch is negative
+        branchLength = 0x4C000000 - branchInstruction;
+        address = reinterpret_cast<uint32_t *>(instructionAddress - branchLength - 0x4);
+    }
+    else
+    {
+        // Branch is positive
+        branchLength = branchInstruction - 0x48000000;
+        address = reinterpret_cast<uint32_t *>(instructionAddress + branchLength - 0x4);
+    }
+    
+    *address = instructions[0];
+    
+    // Clear the cache for both the address and where the instructions were stored
+    clear_DC_IC_Cache(address, sizeof(uint32_t));
+    clear_DC_IC_Cache(instructions, sizeof(uint32_t));
+    
+    // Free the memory used by the trampoline
+    delete[] (instructions);
+    return nullptr;
+}
+
 }
