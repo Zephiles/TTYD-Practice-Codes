4529c00705c79352aa3e34be0cad56d0b403804d
Zephiles <cae222@yahoo.com>
2022-03-13 18:26:36 -0500

    Update/Add win_main functions

--- ttyd-tools/rel/include/ttyd/win_main.h
+++ ttyd-tools/rel/include/ttyd/win_main.h
@@ -18,15 +18,31 @@ extern "C" {
 // winTexSetRot
 // winTexSet
 // winTexInit
-// winFontSetPitch
-// winFontSetLabel
-// winFontSetEdge
+
+#ifdef TTYD_JP
+void winFontSetPitch(float pos[3], float scale[3], uint8_t color[4], float pitch, ...);
+#endif
+
+// label is passed into msgSearch, and the returned string is drawn
+void winFontSetLabel(float pos[3], float scale[3], uint8_t color[4], const char *label);
+
+#ifdef TTYD_JP
+void winFontSetEdge(float pos[3], float scale[3], uint8_t color[4], ...);
+#else
+void winFontSetEdgeWidth(float pos[3], float scale[3], uint8_t color[4], float textWidth, const char *format, ...);
+void winFontSetEdge(float pos[3], float scale[3], uint8_t color[4], const char *format, ...);
+#endif
+
+#ifdef TTYD_EU
+void winFontSetCenter(float pos[3], float scale[3], uint8_t color[4], float center, const char *format, ...);
+#endif
 
 #ifdef TTYD_JP
 void winFontSetR(float pos[3], float scale[3], uint8_t color[4], ...);
 void winFontSet(float pos[3], float scale[3], uint8_t color[4], ...);
 #else
 void winFontSetR(float pos[3], float scale[3], uint8_t color[4], const char *format, ...);
+void winFontSetWidth(float pos[3], float scale[3], uint8_t color[4], float textWidth, const char *format, ...);
 void winFontSet(float pos[3], float scale[3], uint8_t color[4], const char *format, ...);
 #endif
 
