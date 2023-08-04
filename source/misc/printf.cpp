7d9b0f2b2ebd16977d895bbbccdd7373fbdf039f
Zephiles <cae222@yahoo.com>
2023-08-03 19:34:18 -0500

    Adjust vsnprintf to make sure the buffer and maxLength variables are valid before doing anything

--- source/misc/printf.cpp
+++ source/misc/printf.cpp
@@ -9,6 +9,12 @@ extern "C"
 {
     int32_t vsnprintf(char *buffer, std::size_t maxLength, const char *format, va_list args)
     {
+        // Make sure buffer and maxLength are valid before doing anything
+        if (!buffer || (static_cast<int32_t>(maxLength) <= 0))
+        {
+            return 0;
+        }
+
         WriteFormatterFnData data;
         data.buffer = buffer;
         data.maxLength = maxLength;
@@ -17,15 +23,12 @@ extern "C"
         const int32_t ret = __pformatter(__StringWrite, &data, format, args);
 
         // __pformatter does not write a null terminator, so it must be written manually
-        if (buffer)
+        int32_t maxIndex = maxLength - 1;
+        if (ret < maxIndex)
         {
-            int32_t maxIndex = maxLength - 1;
-            if (ret < maxIndex)
-            {
-                maxIndex = ret;
-            }
-            buffer[maxIndex] = '\0';
+            maxIndex = ret;
         }
+        buffer[maxIndex] = '\0';
 
         return ret;
     }
