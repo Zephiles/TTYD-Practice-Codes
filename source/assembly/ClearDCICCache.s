fac57deb1b77f596de391d73f32d250e205b611b
Zephiles <Zephiles@users.noreply.github.com>
2026-04-26 13:39:35 -0500

    ClearDCICCache.s - Fully implement system call interrupt assembly to avoid possible memory barrier issues

--- source/assembly/ClearDCICCache.s
+++ source/assembly/ClearDCICCache.s
@@ -1,4 +1,5 @@
 .global asmClearDCICCache
+.set HID0,1008
 
 asmClearDCICCache:
 stwu %sp,-0x10(%sp)
@@ -9,8 +10,14 @@ stw %r4,0xC(%sp)
 
 # Use DCFlushRangeNoSync to avoid using the system call interrupt
 bl DCFlushRangeNoSync
-isync # Shouldn't be needed but the system call interrupt would do so anyway
+
+# Implement the assembly used for the system call interrupt
+mfspr %r3,HID0
+ori %r4,%r3,0x8
+mtspr HID0,%r4
+isync
 sync
+mtspr HID0,%r3
 
 lwz %r3,0x8(%sp)
 lwz %r4,0xC(%sp)
